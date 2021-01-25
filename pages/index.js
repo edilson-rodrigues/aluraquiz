d90d9cc9f544585f4ea5af4eeafad0909b9e6ee2
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-25 11:58:02 -0300

    create project next

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+/* 
+const Title = (props) => {
+  return (
+    <h1>{props.children}</h1>
+  )
+} */
 
 export default function Home() {
   return <Title>My page</Title>
