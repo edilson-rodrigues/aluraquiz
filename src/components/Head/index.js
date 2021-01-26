c9a90ded9fdb4c36be6efac578f334c1b7eec770
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-26 10:02:24 -0300

    add google font lato

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -28,6 +28,9 @@ function IndexPage() {
                 <link rel="manifest" href="/site.webmanifest" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
+                {/* Fonte */}
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
             </Head>
         </div>
     )
