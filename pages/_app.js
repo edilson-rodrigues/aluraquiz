9bbb3bc7eedea3fed410ceaf2cc4ab5d311cef6f
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-26 20:17:48 -0300

    add tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
 import IndexPage from '../src/components/Head';
+import 'tailwindcss/tailwind.css';
 
 const GlobalStyle = createGlobalStyle`
 * {
