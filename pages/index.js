01a147e5ac34ba409978545fa4e5d098d714e1d0
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-27 14:42:58 -0300

    change text jogar

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
                 value={name}
               />
               <Button type="submit" disabled={isEmptyName}>
-                Vamos jogar
+                Jogar
                 {' '}
                 {!isEmptyName ? name : ''}
               </Button>
