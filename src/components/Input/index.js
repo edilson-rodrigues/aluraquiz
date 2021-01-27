26386a43093893de40afdab87d66fde5c56cb6b1
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-27 14:44:07 -0300

    border radius theme input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,7 +7,7 @@ const InputBase = styled.input`
   padding: 15px;
   font-size: 14px;
   border: 1px solid ${({ theme }) => theme.colors.primary};
-  border-radius: 10px;
+  border-radius: ${({ theme }) => theme.borderRadius};
   color: ${({ theme }) => theme.colors.contrastText};
   background-color: ${({ theme }) => theme.colors.mainBg};
   outline: 0;
