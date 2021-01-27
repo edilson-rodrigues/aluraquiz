ed82438ec66af3da7ce584c20b86b8f2bc0ff712
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-27 14:12:36 -0300

    create input

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -37,6 +38,7 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+
             <form onSubmit={(e) => {
               e.preventDefault();
 
@@ -45,21 +47,16 @@ export default function Home() {
               // router manda para proxima pagina
             }}
             >
-              <div className="pt-6">
-                <input
-                  className="w-full border bg-indigo-200 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-indigo-100  bg-indigo-500 focus:border-transparent rounded placeholder-white"
-                  placeholder="Diz ai seu nome para jogar :)"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Diz ai seu nome para jogar :)"
+              />
+              <button type="submit" disabled={isEmptyName}>
+                Jogar
+                {' '}
+                {!isEmptyName ? name : ''}
+              </button>
 
-              <div className="pt-6">
-                <button className="w-full transition duration-700 ease-in-out bg-gradient-to-r from-pink-500 via-pink-600 to-pink-700 hover:bg-pink-700 rounded justify-center p-2 transform hover:scale-105" type="submit" disabled={isEmptyName}>
-                  Jogar
-                  {' '}
-                  {!isEmptyName ? name : ''}
-                </button>
-              </div>
             </form>
           </Widget.Content>
         </Widget>
---
+++ src/components/Input/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const InputBase = styled.input`
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 10px;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  outline: 0;
+  margin-bottom: 25px;
+`;
+
+const Input = ({ onChange, placeholder, ...props }) => (
+  <div>
+    <InputBase
+      placeholder={placeholder}
+      onChange={onChange}
+      {...props}
+    />
+  </div>
+);
+
+Input.defaultProps = {
+  value: '',
+};
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
+
+export default Input;
