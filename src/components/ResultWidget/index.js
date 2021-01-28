e1ba19178c9496ca0225a083e7ad132ceb7f452d
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-28 18:00:54 -0300

    screen results

--- src/components/ResultWidget/index.js
+++ src/components/ResultWidget/index.js
@@ -1,21 +1,56 @@
 import React from 'react';
+/* Desafio da Ju: Pegue o query param name e exiba o nome da pessoa... */
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
 import Widget from '../Widget';
 import Button from '../Button';
 
-const ResultWidget = ({ results }) => (
-  <Widget>
-    <Widget.Header>
-      Resultado
-    </Widget.Header>
-    <Widget.Content>
-      <div>
-        <p>Mandou bem, name</p>
-        <h2>
-          Você fez
-          {' '}
+const CircleQuestion = styled.div`
+  height: 25px;
+  width: 25px;
+  background-color: ${({ theme }) => theme.colors.secondaryLight};
+  border-radius: 50%;
+  display: inline-block;
+`;
 
-          {/* usando o reduce */}
-          {/* {results.reduce((somatoriaAtual, resultAtual) => {
+const CenterItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BackButton = styled.button`
+  background: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  border: none;
+`;
+
+const ResultWidget = ({ results }) => {
+  const router = useRouter();
+  const { name } = router.query;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        Resultado
+      </Widget.Header>
+      <Widget.Content>
+        <div>
+          <p>
+            Mandou bem,
+            {' '}
+            {/* Desafio da Ju: Pegue o query param name e exiba o nome da pessoa...  */}
+            {name}
+            .
+            {' '}
+            { }
+          </p>
+          <h2>
+            Você fez
+            {' '}
+
+            {/* usando o reduce */}
+            {/* {results.reduce((somatoriaAtual, resultAtual) => {
               const isCorrect = resultAtual === true;
               if (isCorrect) {
                 return somatoriaAtual + 100;
@@ -23,40 +58,55 @@ const ResultWidget = ({ results }) => (
               return somatoriaAtual;
             }, 0)} */}
 
-          {/* usando o filter */}
-          {results.filter((x) => x).length}
-          00
-          {' '}
-          pontos, parabéns!
+            {/* usando o filter */}
+            {results.filter((x) => x).length}
+            00
+            {' '}
+            pontos, parabéns!
 
-        </h2>
-      </div>
-      <Widget>
-        <Widget.Content>
-          {results.map((result, resultIndex) => {
-            const index = resultIndex;
-            return (
-              <Widget.Topic key={index}>
-                {/* number questions */}
-                {`#${index.toString().length > 1
-                  ? index + 1
-                  : `0${index + 1}`} 
+          </h2>
+        </div>
+        <Widget>
+          <Widget.Content>
+            {results.map((result, resultIndex) => {
+              const index = resultIndex;
+              return (
+                <Widget.Topic key={index}>
+                  {/* number questions */}
+                  <CircleQuestion>
+                    <CenterItem>
+                      {`${index.toString().length > 1
+                        ? index + 1
+                        : `0${index + 1}`} 
                     `}
+                    </CenterItem>
 
-                {`Resultado: ${result === true
-                  ? 'Acertou'
-                  : 'Errou'}`}
+                  </CircleQuestion>
 
-              </Widget.Topic>
-            );
-          })}
-          {results}
+                  {` Questão: ${result === true
+                    ? 'Acertou'
+                    : 'Errou'}`}
+
+                </Widget.Topic>
+              );
+            })}
+            {results}
+          </Widget.Content>
+        </Widget>
+        <Button type="submit">ADICIONAR AO MEU PROJETO</Button>
+        <Widget.Content>
+          <CenterItem>
+            <BackButton
+              type="button"
+              onClick={() => router.push('/')}
+            >
+              Voltar para home
+            </BackButton>
+          </CenterItem>
         </Widget.Content>
-      </Widget>
-      <Button type="submit">ADICIONAR AO MEU PROJETO</Button>
-      <p>Voltar para a home</p>
-    </Widget.Content>
-  </Widget>
-);
+      </Widget.Content>
+    </Widget>
+  );
+};
 
 export default ResultWidget;
