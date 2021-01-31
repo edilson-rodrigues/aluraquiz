40937dee35489c8c17813c5f7ba4f0419bfcdb92
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-31 13:57:00 -0300

    aproveitando componente existente

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,77 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
-/* */
-
+import React from 'react';
 import db from '../db.json';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
-import LoadingWidget from '../src/components/Spinner';
-import QuestionWidget from '../src/components/Questions';
-import ResultWidget from '../src/components/ResultWidget';
+import QuizScreen from '../src/screens/Quiz';
 
-const screenStates = {
-  QUIZ: 'QUIZ',
-  LOADING: 'LOADING',
-  RESULT: 'RESULT',
-};
-const QuizPage = () => {
-  const [screenState, setScreenState] = useState(screenStates.LOADING);
-  const [results, setResults] = useState([]);
-  const totalQuestions = db.questions.length;
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const questionIndex = currentQuestion;
-  const question = db.questions[questionIndex];
-
-  function addResult(result) {
-    // results.push(result);
-    setResults([
-      ...results,
-      result,
-    ]);
-  }
-
-  // [React chama de: Efeitos || Effects]
-  // React.useEffect
-  // atualizado === willUpdate
-  // morre === willUnmount
-  useEffect(() => {
-    // fetch() ...
-    setTimeout(() => {
-      setScreenState(screenStates.QUIZ);
-    }, 1 * 1000);
-    // nasce === didMount
-  }, []);
-
-  function handleSubmitQuiz() {
-    const nextQuestion = questionIndex + 1;
-    if (nextQuestion < totalQuestions) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setScreenState(screenStates.RESULT);
-    }
-  }
-
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        {screenState === screenStates.QUIZ && (
-          <QuestionWidget
-            question={question}
-            questionIndex={questionIndex}
-            totalQuestions={totalQuestions}
-            onSubmit={handleSubmitQuiz}
-            addResult={addResult}
-          />
-        )}
-
-        {screenState === screenStates.LOADING && <LoadingWidget />}
-
-        {screenState === screenStates.RESULT && <ResultWidget results={results} />}
-      </QuizContainer>
-    </QuizBackground>
-  );
-};
+const QuizPage = () => (
+  <QuizScreen externalQuestions={db.questions} externalBg={db.bg} />
+);
 
 export default QuizPage;
