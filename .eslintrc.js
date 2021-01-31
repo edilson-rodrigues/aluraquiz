81f653ae3624a15dea8d08fceaf462529530d80b
Edilson Rodrigues <edarodrigs@gmail.com>
2021-01-31 10:48:57 -0300

    linebreak-style 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': 0,
     'react/jsx-props-no-spreading': 0,
+    'linebreak-style': 0,
   },
 };
